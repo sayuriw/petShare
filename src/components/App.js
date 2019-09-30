9452bcc10ae7c50cf6985a3c75ac2f98360587b2
Sayuri <sayuri@MacBook-Pro-72.local>
2019-09-30 17:17:24 +0200

    cleaned up

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,6 @@ import styled from 'styled-components'
 
 export default function App() {
 
-  // const [cards, setCards] = useState({
-  //   "title": "Johny is looking for new friends",
-  //   "description": "My dog Johnny is looking for new friends to take him for a stroll in the park or just chill in the garden. He is very friendly and gets along with other dogs just fine.",
-  //   "tags": ["Dog", "Flexible", "Large"],
-  //   "isBookmarked": "false"
-  // })
-
   return (
     <>
       <AppStyled>
