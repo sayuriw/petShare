f5283d415bda958ebb3b988689e6c440cae73ce6
sayuriw <sayuri.wichmann@gmail.com>
2019-10-11 23:35:29 +0200

    Edit ready

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components/macro'
 import Img from './Img'
 import ReactSVG from 'react-svg'
@@ -6,8 +6,9 @@ import petIcon from '../../data/paw-solid.svg'
 import PropTypes from 'prop-types'
 import Tag from './Tag'
 import { Edit } from 'styled-icons/typicons/Edit'
-import deleteIcon from '../../data/trash-alt-solid.svg'
+import { DeleteForever } from 'styled-icons/material/DeleteForever'
 import { Link } from 'react-router-dom'
+import { Menu } from 'styled-icons/boxicons-regular/Menu'
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
@@ -25,22 +26,31 @@ Card.defaultProps = {
  
 export default function Card({id, picture, title, description, tags, email, isBookmarked, onBookmarkClick, onDeleteClick }) {
   
+  const [isPopped, setIsPopped] = useState(false)
+  
   function handleBookmarkClick(event) {
     event.stopPropagation() 
     onBookmarkClick() 
   }
   function handleDeleteClick(event) {
     event.stopPropagation() 
+    setIsPopped(false)
     onDeleteClick() 
   }
+function handleMenuClick() {
+    setIsPopped(!isPopped)
+}
 
   return (
   <CardStyled>
-    <Img src={picture ? picture : petIcon}/>   
+    <Img src={picture ? picture : petIcon}/>
+    <MenuStyled onClick={handleMenuClick}/>
+    {isPopped && <MenuWrapperStyled> 
+        <DeleteSymbol onClick={handleDeleteClick}>Delete card</DeleteSymbol>
+        <LinkStyled to={{ pathname: "/edit", editCardData: { title, description, email, picture, tags, id }}}><EditSymbol/></LinkStyled>
+    </MenuWrapperStyled>} 
     <TextAreaStyled>
     {Object.values(tags).map(tag => <Tag text={tag} key={tag}/>)}  
-      <DeleteStyled onClick={handleDeleteClick}><ReactSVG src={deleteIcon}/></DeleteStyled>
-      <Link to={{ pathname: "/edit", editCardData: { title, description, email, picture, tags, id }}}><EditSymbol/></Link>
       <h1>{title}</h1>
       <p>{description}</p>
       <ButtonsAreaStyled>
@@ -56,28 +66,42 @@ const CardStyled = styled.section`
   background: #FFFFFF;
   border-radius: 5px;
   box-shadow: 0 10px 10px #0002;
+  position: relative;
   `
-
+const MenuStyled = styled(Menu)`
+position: absolute;
+height:35px;
+width: 35px;
+right: 10px;
+`
+const MenuWrapperStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  right: 10px;
+  top: 32px;
+`
+const DeleteSymbol = styled(DeleteForever)`
+  height:35px;
+  width: 35px;
+`
+const EditSymbol = styled(Edit)`
+  height:35px;
+  width: 35px;
+`
+const LinkStyled = styled(Link)`
+  text-decoration: none;
+  color: black;
+`
 const TextAreaStyled = styled.div`
   margin: 10px;
   margin-bottom: 0;
   position: relative;
 `
-
-const DeleteStyled = styled.div`
-  position: absolute;
-  width: 30px;
-  height: 30px;
-  right: -5px;
-  top: -10px;
-  color: #83b0ea;
-`
-
 const ButtonsAreaStyled = styled.div`
   position: relative;
   padding-bottom: 20px;
 `
-
 const BookmarkStyled = styled.div`
   position: absolute;
   color: ${({ active }) => (active ? '#e6e04a' : '#83b0ea')};
@@ -91,7 +115,6 @@ const ContactMeStyled = styled.a`
   padding-left: 20px;
   color: #83b0ea;
 `
-const EditSymbol = styled(Edit)`
-  height:35px;
-  width: 35px;
-`
+
+
+
--- src/components/cards/Img.js
+++ src/components/cards/Img.js
@@ -7,6 +7,7 @@ const Img = styled.img`
   border-top-right-radius: 6px;
   box-shadow: 0 10px 10px #0002;
   background-color: gray;
+  position: relative;
 `
 
 export default Img
