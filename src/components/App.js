109ef2988d39f4ec31777cf487a4b336677fb5dc
sayuriw <sayuri.wichmann@gmail.com>
2019-10-16 18:09:43 +0200

    WIP cleanUp

--- src/components/App.js
+++ src/components/App.js
@@ -1,131 +1,79 @@
-import React, {useState, useEffect} from 'react'
-import CardPage from './cards/CardPage'
+import React, { useState, useEffect, useContext } from 'react'
+import EditCardPage from './EditCardPage'
+import CardsListPage from './CardsListPage'
 import styled from 'styled-components'
 import NavBar from './Navbar'
-import EditCardPage from './EditCardPage'
+// import EditCardPage from './EditCardPage'
 import CreateCardPage from './CreateCardPage'
-import { getCards, patchCard, postCard, deleteCard } from './cards/services'
-import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom'
+import {
+  getCards,
+  patchCard,
+  postCard,
+  deleteCard
+} from '../components/cards/services'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+export const PetsContext = React.createContext([[], () => {}])
 
-export default function App() {
-
+function PetsProvider({ children }) {
   const [pets, setPets] = useState([])
-  const [petsFiltered, setPetsFiltered] = useState(pets)
-  const [selectedFilter, setSelectedFilter] = useState('all')
-  const [selectedTag, setSelectedTag] = useState('')
-
-  useEffect(() => {
-    getCards(pets)
-    .then(
-      (pets) => {pets.sort((a,b) => {
-      const dateA = new Date(b.createdDate).getTime()
-      const dateB = new Date(a.createdDate).getTime()
-      return dateA < dateB ? -1 : dateA > dateB ? 1 : 0})
-      setPets(pets)
-      })
-    }, [])
-  
-
-  useEffect(() => {
-    handleTagClick(selectedFilter, selectedTag)
-  }, [pets])
-
-  const allTags = {}
-  pets.forEach(pet => {
-    Object.keys(pet.tags).forEach(tag => {
-      allTags[tag] === undefined
-        ? allTags[tag] = [pet.tags[tag]]
-        : allTags[tag] = Array.from(new Set([...allTags[tag], pet.tags[tag]]))
-    })
-  })
-
-  const HomePage = withCardPage('PetShare')
-  const FavoritesPage = withCardPage('Favorites', 'isBookmarked')
 
   return (
-    <Router>
-      <AppStyled>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/favorites" component={FavoritesPage} />
-          <Route path="/newCard" render={() => 
-            <CreateCardPage title="Create a new PetCard" onSubmit={createCard}/>}
-          />
-          <Route path="/edit" render={(props) => {
-            return <EditCardPage onSubmit={handleEditClick} editCardData={props.location.editCardData}/>}}
-          />
-        
-        </Switch>
-        <NavBar/>
-      </AppStyled>
-    </Router>
+    <PetsContext.Provider value={[pets, setPets]}>
+      {children}
+    </PetsContext.Provider>
   )
-  
+}
+
+export default function App() {
+  const [pets, setPets] = useContext(PetsContext)
+
+  return (
+    <PetsProvider>
+      <Router>
+        <AppStyled>
+          <Switch>
+            <Route exact path="/" render={() => <CardsListPage />} />
+            <Route
+              exact
+              path="/favorites"
+              render={() => {
+                return <CardsListPage showOnlyBookmarks={true} />
+              }}
+            />
+            <Route
+              path="/newCard"
+              render={() => <CreateCardPage onSubmit={createCard} />}
+            />
+            <Route
+              path="/edit"
+              render={props => {
+                return (
+                  <EditCardPage
+                    onSubmit={handleEditClick}
+                    editCardData={props.location.editCardData}
+                  />
+                )
+              }}
+            />
+          </Switch>
+          <NavBar />
+        </AppStyled>
+      </Router>
+    </PetsProvider>
+  )
+
+  function handleEditClick(id, editData) {
+    patchCard(id, editData).then(editPet => {
+      const index = pets.findIndex(pet => pet._id === editPet._id)
+      setPets([...pets.slice(0, index), editPet, ...pets.slice(index + 1)])
+    })
+  }
+
   function createCard(cardData) {
     postCard(cardData).then(pet => {
       setPets([pet, ...pets])
     })
-  }
-
-  function handleTagClick(selectedFilter, clickedTag) {
-    setSelectedTag(clickedTag)
-    setSelectedFilter(selectedFilter)
-    if (selectedFilter === 'all') {
-      setPetsFiltered(pets)
-    } else {
-      const newPetsFiltered = pets.filter(pet => pet.tags[selectedFilter] === clickedTag)
-      setPetsFiltered(newPetsFiltered)
-    }
-  }
-
-  function handleDeleteClick(pet) {
-    if (window.confirm('Are you sure you wish to delete this item?')) {
-    deleteCard(pet._id).then(deletedPet => {
-      const index = pets.findIndex(pet => pet._id === deletedPet._id)
-    setPets ([
-        ...pets.slice(0, index),
-        ...pets.slice(index + 1)
-      ])
-    })
-   }
-  }
-
-  function handleBookmarkClick(pet) {
-    patchCard(pet._id, { isBookmarked: !pet.isBookmarked }).then(updatedPet => {
-      const index = pets.findIndex(pet => pet._id === updatedPet._id)
-    setPets ([
-      ...pets.slice(0, index),
-      { ...pet, isBookmarked: !pet.isBookmarked },
-      ...pets.slice(index + 1),
-      ])
-    })
-  }
-  function handleEditClick(id, editData) {
-    patchCard(id, editData)
-    .then(editPet => {
-      const index = pets.findIndex(pet => pet._id === editPet._id)
-      setPets([
-        ...pets.slice(0, index),
-        editPet,
-        ...pets.slice(index + 1)
-      ])
-    })
-  }
-  
-  function withCardPage(title, filterProp) {
-    return () => {
-      const petsFilteredByProp = filterProp ? petsFiltered.filter(pet => pet[filterProp]) : petsFiltered
-      return <CardPage title={title}
-                       tags={allTags} 
-                       onTagClick={handleTagClick}
-                       activeTag={selectedTag}
-                       onBookmarkClick={handleBookmarkClick} 
-                       onDeleteClick={handleDeleteClick}
-                       onEditClick={handleEditClick}
-                       pets={petsFilteredByProp}/>
-
-    }
   }
 }
 
@@ -138,4 +86,135 @@ const AppStyled = styled.div`
   top: 0;
   bottom: 0;
   height: 100%;
-  `
+`
+
+// export default function App() {
+
+//   const [pets, setPets] = useState([])
+//   const [petsFiltered, setPetsFiltered] = useState(pets)
+//   const [selectedFilter, setSelectedFilter] = useState('all')
+//   const [selectedTag, setSelectedTag] = useState('')
+
+//   useEffect(() => {
+//     getCards(pets)
+//     .then(
+//       (pets) => {pets.sort((a,b) => {
+//       const dateA = new Date(b.createdDate).getTime()
+//       const dateB = new Date(a.createdDate).getTime()
+//       return dateA < dateB ? -1 : dateA > dateB ? 1 : 0})
+//       setPets(pets)
+//       })
+//     }, [])
+
+//   useEffect(() => {
+//     handleTagClick(selectedFilter, selectedTag)
+//   }, [pets])
+
+//   const allTags = {}
+//   pets.forEach(pet => {
+//     Object.keys(pet.tags).forEach(tag => {
+//       allTags[tag] === undefined
+//         ? allTags[tag] = [pet.tags[tag]]
+//         : allTags[tag] = Array.from(new Set([...allTags[tag], pet.tags[tag]]))
+//     })
+//   })
+
+// const HomePage = withCardPage('PetShare')
+// const FavoritesPage = withCardPage('Favorites', 'isBookmarked')
+
+//   return (
+//     <Router>
+//       <AppStyled>
+//         <Switch>
+//           <Route exact path="/" component={HomePage} />
+//           <Route path="/favorites" component={FavoritesPage} />
+//           <Route path="/newCard" render={() =>
+//             <CreateCardPage title="Create a new PetCard" onSubmit={createCard}/>}
+//           />
+//           <Route path="/edit" render={(props) => {
+//             return <EditCardPage onSubmit={handleEditClick} editCardData={props.location.editCardData}/>}}
+//           />
+
+//         </Switch>
+//         <NavBar/>
+//       </AppStyled>
+//     </Router>
+//   )
+
+//   function createCard(cardData) {
+//     postCard(cardData).then(pet => {
+//       setPets([pet, ...pets])
+//     })
+//   }
+
+//   function handleTagClick(selectedFilter, clickedTag) {
+//     setSelectedTag(clickedTag)
+//     setSelectedFilter(selectedFilter)
+//     if (selectedFilter === 'all') {
+//       setPetsFiltered(pets)
+//     } else {
+//       const newPetsFiltered = pets.filter(pet => pet.tags[selectedFilter] === clickedTag)
+//       setPetsFiltered(newPetsFiltered)
+//     }
+//   }
+
+//   function handleDeleteClick(pet) {
+//     if (window.confirm('Are you sure you wish to delete this item?')) {
+//     deleteCard(pet._id).then(deletedPet => {
+//       const index = pets.findIndex(pet => pet._id === deletedPet._id)
+//     setPets ([
+//         ...pets.slice(0, index),
+//         ...pets.slice(index + 1)
+//       ])
+//     })
+//    }
+//   }
+
+//   function handleBookmarkClick(pet) {
+//     patchCard(pet._id, { isBookmarked: !pet.isBookmarked }).then(updatedPet => {
+//       const index = pets.findIndex(pet => pet._id === updatedPet._id)
+//     setPets ([
+//       ...pets.slice(0, index),
+//       { ...pet, isBookmarked: !pet.isBookmarked },
+//       ...pets.slice(index + 1),
+//       ])
+//     })
+//   }
+//   function handleEditClick(id, editData) {
+//     patchCard(id, editData)
+//     .then(editPet => {
+//       const index = pets.findIndex(pet => pet._id === editPet._id)
+//       setPets([
+//         ...pets.slice(0, index),
+//         editPet,
+//         ...pets.slice(index + 1)
+//       ])
+//     })
+//   }
+
+//   function withCardPage(title, filterProp) {
+//     return () => {
+//       const petsFilteredByProp = filterProp ? petsFiltered.filter(pet => pet[filterProp]) : petsFiltered
+//       return <CardPage title={title}
+//                        tags={allTags}
+//                        onTagClick={handleTagClick}
+//                        activeTag={selectedTag}
+//                        onBookmarkClick={handleBookmarkClick}
+//                        onDeleteClick={handleDeleteClick}
+//                        onEditClick={handleEditClick}
+//                        pets={petsFilteredByProp}/>
+
+//     }
+//   }
+// }
+
+// const AppStyled = styled.div`
+//   display: grid;
+//   grid-template-rows: auto 48px;
+//   position: fixed;
+//   left: 0;
+//   right: 0;
+//   top: 0;
+//   bottom: 0;
+//   height: 100%;
+//   `
--- src/components/cards/CardPage.js
+++ src/components/cards/CardPage.js
@@ -1,50 +1,50 @@
-import React from 'react'
-import Card from './Card'
-import Filter from '../Filter'
-import Page from '../../common/Page'
-import PropTypes from 'prop-types'
-import styled from 'styled-components/macro'
-import logo from '../../data/petshare.png'
+// import React from 'react'
+// import Card from './Card'
+// import Filter from '../Filter'
+// import Page from '../../common/Page'
+// import PropTypes from 'prop-types'
+// import styled from 'styled-components/macro'
+// import logo from '../../data/petshare.png'
 
-CardPage.propTypes = {
-  title: PropTypes.string.isRequired,
-  onBookmarkClick: PropTypes.func,
-  onDeleteClick: PropTypes.func,
-  pets: PropTypes.array,
-  onTagClick:PropTypes.func,
-  tags:PropTypes.object,
-  activeTag: PropTypes.string,
-}  
+// CardPage.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   onBookmarkClick: PropTypes.func,
+//   onDeleteClick: PropTypes.func,
+//   pets: PropTypes.array,
+//   onTagClick:PropTypes.func,
+//   tags:PropTypes.object,
+//   activeTag: PropTypes.string,
+// }  
 
-export default function CardPage({ onBookmarkClick, onDeleteClick, onEditClick, pets, onTagClick, tags, activeTag}) {
-  return (
-    <Page title={logo}>
-      <Scroller>
-        <Filter tags={tags} activeTag={activeTag} onTagClick={onTagClick} />
-      {pets.map((pet) => (
-        <Card
-          key={pet._id}
-          id={pet._id}
-          tags={pet.tags}
-          title={pet.title}
-          description={pet.description}
-          onBookmarkClick={() => onBookmarkClick(pet)}
-          onDeleteClick={() => onDeleteClick(pet)}
-          onEditClick={() => onEditClick(pet)}
-          isBookmarked={pet.isBookmarked}
-          picture={pet.picture}
-          email={pet.email}
-        />
+// export default function CardPage({ onBookmarkClick, onDeleteClick, onEditClick, pets, onTagClick, tags, activeTag}) {
+//   return (
+//     <Page title={logo}>
+//       <Scroller>
+//         <Filter tags={tags} activeTag={activeTag} onTagClick={onTagClick} />
+//       {pets.map((pet) => (
+//         <Card
+//           key={pet._id}
+//           id={pet._id}
+//           tags={pet.tags}
+//           title={pet.title}
+//           description={pet.description}
+//           onBookmarkClick={() => onBookmarkClick(pet)}
+//           onDeleteClick={() => onDeleteClick(pet)}
+//           onEditClick={() => onEditClick(pet)}
+//           isBookmarked={pet.isBookmarked}
+//           picture={pet.picture}
+//           email={pet.email}
+//         />
         
-      ))}
-      </Scroller> 
-   </Page>
-  )
-}
-const Scroller = styled.div`
-  display: grid;
-  gap: 20px;
-  overflow-y: auto;
-  scroll-behavior: smooth;
-  padding: 20px 10px;
-`
+//       ))}
+//       </Scroller> 
+//    </Page>
+//   )
+// }
+// const Scroller = styled.div`
+//   display: grid;
+//   gap: 20px;
+//   overflow-y: auto;
+//   scroll-behavior: smooth;
+//   padding: 20px 10px;
+// `
